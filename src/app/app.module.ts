27670076053d496528ce58d46d3bd0af39dd02dc
Esapat <samrickky@gmail.com>
2019-11-09 16:36:21 +0300

    Creating the Navigation using the Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes} from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {path: 'products', component: 'ProductListComponent'},
+  {path: 'product/id', component: 'ProductPageComponent'},
+  {path: '', pathMatch: 'full', redirectTo: 'products'}
+];
 
 @NgModule({
   declarations: [
@@ -19,6 +27,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
