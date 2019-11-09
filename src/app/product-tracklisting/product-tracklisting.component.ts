7807face211a2462b57d27f19b8f5e5c4dd4cbc4
Esapat <samrickky@gmail.com>
2019-11-09 13:11:53 +0300

    Adding the Tracklisting component

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Album } from '../album';
+import { ProductService } from '../product.service';
+
 
 @Component({
   selector: 'app-product-tracklisting',
   templateUrl: './product-tracklisting.component.html',
   styleUrls: ['./product-tracklisting.component.css']
 })
+
+@Inject(ProductService)
 export class ProductTracklistingComponent implements OnInit {
 
-  constructor() { }
+   albumInfo: Album;
+
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
